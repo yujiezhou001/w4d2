bc8ee06b6cdcc07d9016b12f7a986a07a422f662
Gordon <gordonzhou68@outlook.com>
2019-05-22 00:46:59 +0000

    milestones table modified

--- migrations/20190521235917_create_addforeignkey.js
+++ migrations/20190521235917_create_addforeignkey.js
@@ -1,7 +1,7 @@
 
 exports.up = function(knex, Promise) {
     return knex.schema.table('milestones', function(table) {
-        table.integer('famous_person_id').notNull().defaultTo(0);
+        table.integer('famous_person_id').notNull();
         table.foreign('famous_person_id').references('id').on('famous_people').onDelete('cascade');
     });
 };
